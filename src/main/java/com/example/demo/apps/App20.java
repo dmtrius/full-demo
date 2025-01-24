928edf75e5f7dbc1be677d1fda1b4e70296c0ac9
Dmytro Gordiienko <dmitry.gordienko@gmail.com>
2025-01-24 11:54:45 +0100

    added Future testing

--- src/main/java/com/example/demo/apps/App20.java
+++ src/main/java/com/example/demo/apps/App20.java
@@ -1,45 +1,88 @@
 package com.example.demo.apps;
 
+import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.Observer;
 import io.reactivex.rxjava3.disposables.Disposable;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
 public class App20 {
     void main() {
-        testObservable();
+//        testObservable();
+        testFutures();
+    }
+
+    @SneakyThrows
+    private static void testFutures() {
+        // Create an ExecutorService with a single thread
+        try (ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor()) {
+            // Submit a task to the ExecutorService
+            Future<Integer> future = executorService.submit(() -> {
+                TimeUnit.SECONDS.sleep(1); // Simulate a time-consuming operation
+                return 9; // The result of the computation
+            });
+            // Continue with other tasks while the above task is running
+            log.info("working...");
+            Thread.currentThread().join(2000);
+            // Check if the task is done
+            if (future.isDone()) {
+                try {
+                    // Retrieve the result when the task is complete
+                    Integer result = future.get();
+                    log.info("Task completed. Result: {}", result);
+                } catch (InterruptedException | ExecutionException e) {
+                    log.error(e.getMessage());
+                }
+            } else {
+                log.info("Task is not yet complete. Continuing with other tasks...");
+            }
+
+            // Shutdown the ExecutorService when done
+            executorService.shutdown();
+        }
     }
 
     private static void testObservable() {
         // Create an Observable that emits a sequence of some great authors
-        Observable<String> observable = Observable.create(emitter -> {
+        Observable<String> observable
+                = Observable.create(emitter -> {
             emitter.onNext("Charles Dickens");
             emitter.onNext("Jonathan Swift");
             emitter.onNext("Chinua Achebe");
             emitter.onNext("William Shakespear");
             emitter.onNext("Thomas Paine");
+            emitter.onNext("George Orwell");
             emitter.onComplete(); // Indicates that the observable has completed
         });
 
         // Create an Observer to subscribe to the Observable
         Observer<String> observer = new Observer<>() {
             @Override
-            public void onSubscribe(Disposable d) {
-                System.out.println("Subscribed to the Observable.");
+            public void onSubscribe(@NonNull Disposable d) {
+                log.info("Subscribed to the Observable.");
             }
 
             @Override
-            public void onNext(String value) {
-                System.out.println("Received: " + value);
+            public void onNext(@NonNull String value) {
+                log.info("Received: {}", value);
             }
 
             @Override
             public void onError(Throwable e) {
-                System.err.println("Error: " + e.getMessage());
+                log.error("Error: {}", e.getMessage());
             }
 
             @Override
             public void onComplete() {
-                System.out.println("Observable completed.");
+                log.info("Observable completed.");
             }
         };
 
---
+++ src/main/java/com/example/demo/apps/App21.java
@@ -0,0 +1,7 @@
+package com.example.demo.apps;
+
+public class App21 {
+    void main() {
+
+    }
+}
