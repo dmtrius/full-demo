e4c11e30d96972b131c49869f6883e75b5703dfa
Dmytro Gordiienko <d.gordiienko@playsmart.io>
2025-07-30 20:14:11 +0200

    update

--- src/main/java/com/example/demo/apps/App30.java
+++ src/main/java/com/example/demo/apps/App30.java
@@ -1,9 +1,17 @@
 package com.example.demo.apps;
 
+import lombok.SneakyThrows;
+
 import static java.io.IO.println;
 
 public class App30 {
+    @SneakyThrows
     void main() {
+        println("brrr");
+        processInfo();
+    }
 
+    void processInfo() {
+        ProcessHandle.current().info().command().ifPresent(System.out::println);
     }
 }
