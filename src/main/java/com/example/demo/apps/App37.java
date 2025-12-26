7c8a14590b2038b10633a346177f1220bc8d5d72
Dmytro Gordiienko <dmitry.gordienko@gmail.com>
2025-12-26 17:57:25 +0100

    updates

--- src/main/java/com/example/demo/apps/App37.java
+++ src/main/java/com/example/demo/apps/App37.java
@@ -1,26 +1,7 @@
 package com.example.demo.apps;
 
-import java.util.List;
-
-import static java.lang.IO.println;
-
 public class App37 {
     void main() {
-        m1();
-    }
 
-    private static void m2() {
-
-    }
-
-    private static void m1() {
-        List<Integer> l = List.of(1, 2, 3, 4, 5, 6, 7);
-        List<Integer> r = l.stream()
-                .filter(i -> i % 2 == 0)
-                .toList()
-                .stream()
-                .filter(i -> i > 4)
-                .toList();
-        println(r);
     }
 }
