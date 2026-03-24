c3623f853332c13f3de90328d877aeaa9a46471f
Dmytro Gordiienko <dmtrius@protonmail.com>
2026-03-24 20:14:13 +0100

    update

--- src/main/java/com/example/demo/apps/App41.java
+++ src/main/java/com/example/demo/apps/App41.java
@@ -1,65 +1,50 @@
 package com.example.demo.apps;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
 import static java.lang.IO.println;
 
 public class App41 {
     void main() {
-        println(longestStringWithNoRepetition("abcabcbb"));
-        println(longestStringWithNoRepetition("bbbbb"));
-        println(longestStringWithNoRepetition("pwwkew"));
+        println(lengthOfLongestSubstring("abcabcbb"));
+        println(lengthOfLongestSubstring("bbbbb"));
+        println(lengthOfLongestSubstring("pwwkew"));
     }
 
-    int longestStringWithNoRepetition(String s) {
-        char[] input = s.toCharArray();
-        int start = 0;
-        int end = 1;
-        char current = input[0];
-        List<Character> present = new LinkedList<>();
-        present.add(current);
-        int longest = end - start;
-        for (int i = 1; i < input.length; ++i) {
-            current = input[i];
-            end = i;
-            if (!present.contains(current)) {
-                present.add(current);
-                if (longest < end - start) {
-                    longest = end - start;
-                }
-            } else {
-                start++;
-                present.clear();
-            }
-        }
-        if (!present.isEmpty()) {
-            return end - start;
-        } else {
+    int lengthOfLongestSubstring(String s) {
+        if (Objects.isNull(s) || s.isBlank()) {
             return 0;
         }
+
+        Set<Character> seen = new HashSet<>();
+        int left = 0;
+        int maxLength = 0;
+
+        for (int right = 0; right < s.length(); ++right) {
+            char c = s.charAt(right);
+            while (seen.contains(c)) {
+                seen.remove(s.charAt(left));
+                ++left;
+            }
+            seen.add(c);
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
     }
 }
 
-/**
- * “Given a string s, find the length of the longest substring without repeating characters.
- *
- * Input:  s = "abcabcbb"
- *
- * Output: 3
- *
- * Explanation: "abc" is the longest substring without repeating characters.
- *
- * Input:  s = "bbbbb"
- *
- * Output: 1
- *
- * Explanation: "b"
- *
- * Input:  s = "pwwkew"
- *
- * Output: 3
- *
- * Explanation: "wke"
- *
+/*
+Given a string s, find the length of the longest substring without repeating characters.
+Input:  s = "abcabcbb"
+Output: 3
+Explanation: "abc" is the longest substring without repeating characters.
+Input:  s = "bbbbb"
+Output: 1
+Explanation: "b"
+Input:  s = "pwwkew"
+Output: 3
+Explanation: "wke"
  */
