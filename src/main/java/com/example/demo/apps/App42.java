44f6c70b69221d9b1cbe65fd7d87d1ddb86cda4c
Dmytro Gordiienko <d.gordiienko@playsmart.io>
2026-03-24 19:58:18 +0100

    update

--- src/main/java/com/example/demo/apps/App42.java
+++ src/main/java/com/example/demo/apps/App42.java
@@ -1,25 +1,9 @@
 package com.example.demo.apps;
 
-import java.util.HashMap;
-import java.util.Map;
+import static java.lang.IO.println;
 
 public class App42 {
     void main() {
-
-    }
-
-    private static final int MAX_KEY_SIZE = 20;
-    private Map<String, String> storage = new HashMap<>();
-
-    private String kvStorage(String key, String secret) {
-        if (key.length() > MAX_KEY_SIZE) {
-            throw new RuntimeException("Max key length is " + MAX_KEY_SIZE);
-        }
-
-        if (storage.containsKey(key)) {
-            throw new RuntimeException("");
-        }
-
-        return key;
+        println("Arghhh");
     }
 }
