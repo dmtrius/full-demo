385001d50d7b79a794c7555e42d5ec0a329e9ee6
Dmytro Gordiienko <dmitry.gordienko@gmail.com>
2026-03-29 23:02:23 +0200

    updates

--- src/main/java/com/example/demo/apps/App43.java
+++ src/main/java/com/example/demo/apps/App43.java
@@ -1,9 +1,19 @@
 package com.example.demo.apps;
 
+import java.util.logging.Logger;
+
 import static java.lang.IO.println;
 
 public class App43 {
     void main() {
         println("Arghhh");
+        m1();
+    }
+
+    private static final LazyConstant<Logger> LOG =
+            LazyConstant.of(() -> Logger.getLogger(App43.class.getName()));
+
+    void m1() {
+        LOG.get().info("==== LazyConstant ===");
     }
 }
