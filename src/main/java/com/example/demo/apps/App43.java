bef5a2bca65ec494e5e62af464ac164b8fd8ecc5
Dmytro Gordiienko <dmtrius@protonmail.com>
2026-03-30 13:34:55 +0200

    update

--- src/main/java/com/example/demo/apps/App43.java
+++ src/main/java/com/example/demo/apps/App43.java
@@ -2,11 +2,8 @@ package com.example.demo.apps;
 
 import java.util.logging.Logger;
 
-import static java.lang.IO.println;
-
 public class App43 {
     void main() {
-        println("Arghhh");
         m1();
     }
 
