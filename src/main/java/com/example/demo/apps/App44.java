b586ac406766f56779dd1eaa33c401450b66a912
Dmytro Gordiienko <dmitry.gordienko@gmail.com>
2026-03-30 14:29:33 +0200

    updates

--- src/main/java/com/example/demo/apps/App44.java
+++ src/main/java/com/example/demo/apps/App44.java
@@ -2,12 +2,20 @@ package com.example.demo.apps;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.lang.invoke.MethodHandles;
+import java.util.logging.Logger;
+
 import static java.lang.IO.println;
 
 @Slf4j
 public class App44 {
+
+    private static final Logger LOGGER = Logger.getLogger(
+            MethodHandles.lookup().lookupClass().getName());
+
     void main() {
         println("Arghhhh");
-        log.info("Whooo");
+        log.info("lombok: Whooo");
+        LOGGER.info("LOGGER: blyaaa");
     }
 }
