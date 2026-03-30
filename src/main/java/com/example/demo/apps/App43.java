d71c14b99c001025ad1bbc9b1df9f2d7758810bf
Dmytro Gordiienko <dmitry.gordienko@gmail.com>
2026-03-30 13:57:59 +0200

    updates

--- src/main/java/com/example/demo/apps/App43.java
+++ src/main/java/com/example/demo/apps/App43.java
@@ -1,16 +1,27 @@
 package com.example.demo.apps;
 
+import java.lang.invoke.MethodHandles;
 import java.util.logging.Logger;
+
+import static java.lang.IO.println;
 
 public class App43 {
     void main() {
         m1();
+        println(m2());
     }
 
     private static final LazyConstant<Logger> LOG =
-            LazyConstant.of(() -> Logger.getLogger(App43.class.getName()));
+            LazyConstant.of(() -> Logger.getLogger(
+                    MethodHandles.lookup().lookupClass().getName()));
 
     void m1() {
         LOG.get().info("==== LazyConstant ===");
+    }
+
+    String m2() {
+        return Thread.currentThread()
+                .getStackTrace()[1]
+                .getClassName();
     }
 }
--- src/main/java/com/example/demo/apps/App44.java
+++ src/main/java/com/example/demo/apps/App44.java
@@ -1,9 +1,13 @@
 package com.example.demo.apps;
 
+import lombok.extern.slf4j.Slf4j;
+
 import static java.lang.IO.println;
 
+@Slf4j
 public class App44 {
     void main() {
         println("Arghhhh");
+        log.info("Whooo");
     }
 }
