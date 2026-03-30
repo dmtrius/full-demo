a535507ebb24f7a2131d2aec965235293ae15fe0
Dmytro Gordiienko <dmitry.gordienko@gmail.com>
2026-03-30 12:31:50 +0200

    updates

--- src/main/java/com/example/demo/apps/StructuredConcurrencyExample.java
+++ src/main/java/com/example/demo/apps/StructuredConcurrencyExample.java
@@ -9,7 +9,7 @@ public class StructuredConcurrencyExample {
     void main() {
         try (var scope = StructuredTaskScope.open()) {
             var result1 = scope.fork(() -> {
-                if (Math.random() > 0.6)  {
+                if (Math.random() > 0.3d)  {
                     throw new SCException("Task 1 failed");
                 }
                 return "Task 1 completed";
@@ -21,7 +21,7 @@ public class StructuredConcurrencyExample {
 
             println(result1.get());
             println(result2.get());
-        } catch (InterruptedException e) {
+        } catch (InterruptedException | StructuredTaskScope.FailedException  e) {
             Thread.currentThread().interrupt();
             println("Error in one of the tasks: " + e.getMessage());
         }
