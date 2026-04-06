21323ee6948f42f73e0a5d40c523e34fdce95c89
Dmytro Gordiienko <dmitry.gordienko@gmail.com>
2026-04-06 11:53:02 +0200

    updates

--- src/main/java/com/example/demo/apps/App45.java
+++ src/main/java/com/example/demo/apps/App45.java
@@ -1,5 +1,10 @@
 package com.example.demo.apps;
 
+import com.github.f4b6a3.ulid.Ulid;
+import com.github.f4b6a3.uuid.UuidCreator;
+
+import java.util.UUID;
+
 import static java.lang.IO.println;
 
 public class App45 {
@@ -9,6 +14,18 @@ public class App45 {
     }
 
     void m1() {
+        Ulid ulid = Ulid.fast();
+        String id = ulid.toString();
+        println("ULID" + id);
 
+        UUID v4 = UUID.randomUUID();
+        println(v4);
+        println("Variant: " + v4.variant());
+        println("Version: " + v4.version());
+
+        UUID v7 = UuidCreator.getTimeOrderedEpoch();
+        println(v7);
+        println("Variant: " + v7.variant());
+        println("Version: " + v7.version());
     }
 }
