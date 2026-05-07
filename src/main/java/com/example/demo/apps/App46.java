8e095e0d1f0090cf88692995159eb7b76d75402f
Dmytro Gordiienko <dmitry.gordienko@gmail.com>
2026-05-07 06:50:11 +0200

    updates

--- src/main/java/com/example/demo/apps/App46.java
+++ src/main/java/com/example/demo/apps/App46.java
@@ -1,13 +1,33 @@
 package com.example.demo.apps;
 
+import org.jspecify.annotations.NonNull;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Gatherers;
+
 import static java.lang.IO.println;
 
 public class App46 {
     void main() {
-        m1();
+        m1(UUID.randomUUID().toString());
     }
 
-    void m1() {
-        println("Arghhh");
+    /// m1() for practise some code
+    ///
+    /// Example:
+    /// ```java
+    /// m1("param");
+    /// ```
+    ///
+    /// @param param the PARAM
+    /// @return void
+    void m1(@NonNull String param) {
+        println(param);
+        List<Integer> list = List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+        var windows = list.stream()
+                .gather(Gatherers.windowSliding(3))
+                .toList();
+        windows.forEach(IO::println);
     }
 }
