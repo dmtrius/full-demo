a415b8fcb9480ba763f675ab27eb6ee155674674
Dmytro Gordiienko <dmtrius@protonmail.com>
2026-05-07 05:56:18 +0200

    update

--- src/main/java/com/example/demo/apps/App40.java
+++ src/main/java/com/example/demo/apps/App40.java
@@ -4,6 +4,12 @@ import static java.lang.IO.println;
 
 public class App40 {
     void main() {
+        String str = "Hello, World!";
+        int length = m1(str);
+        println("Length of the string: " + length);
+    }
 
+    public int m1(String str) {
+        return str.length();
     }
 }
--- src/test/java/com/example/demo/apps/App40Test.java
+++ src/test/java/com/example/demo/apps/App40Test.java
@@ -7,4 +7,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class App40Test {
     private final App40 app = new App40();
 
+    @Test
+    void test_m1() {
+        assertEquals(5, app.m1("Hello"));
+    }
 }
