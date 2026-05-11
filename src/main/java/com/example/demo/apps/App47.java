ddfb6e094232f8f3bc360cacccfd3710044b05c2
Dmytro Gordiienko <d.gordiienko@playsmart.io>
2026-05-11 22:40:45 +0200

    update

--- src/main/java/com/example/demo/apps/App47.java
+++ src/main/java/com/example/demo/apps/App47.java
@@ -1,6 +1,6 @@
 package com.example.demo.apps;
 
-import java.net.URL;
+import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.MessageFormat;
@@ -9,10 +9,12 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 import javax.xml.parsers.DocumentBuilderFactory;
+
 import org.w3c.dom.Document;
 
 import static java.lang.IO.println;
@@ -20,19 +22,25 @@ import static java.lang.IO.println;
 public class App47 {
     void main() throws Exception {
         // 1. Multiple Each Item in a List by 2
-        IntStream.range(1, 10).map(i -> i * 2);
+        var list = IntStream.range(1, 10).map(i -> i * 2);
+        IO.println(list);
 
         // 2. Sum a List of Numbers
-        IntStream.range(1, 1000).sum();
+        var sum = IntStream.range(1, 1000).sum();
+        IO.println(sum);
 
         // 3. Verify if Exists in a String
         List<String> wordList = Arrays.asList("java", "jdk", "spring", "maven");
         String tweet = "This is an example tweet talking about java and maven.";
-        wordList.stream().anyMatch(tweet::contains);
+        var words = wordList.stream().anyMatch(tweet::contains);
+        IO.println(words);
 
         // 4. Read in a File
-        String fileText = new String(Files.readAllBytes(Paths.get("data.txt")));
-        List<String> fileLines = Files.readAllLines(Paths.get("data.txt"));
+        var url = getClass().getClassLoader().getResource("data.txt");
+        String fileText = new String(Files.readAllBytes(Paths.get(url.toURI())));
+        List<String> fileLines = Files.readAllLines(Paths.get(url.toURI()));
+        IO.println(fileText);
+        IO.println(fileLines);
 
         // 5. Happy Birthday to You!
         IntStream.rangeClosed(1, 4).mapToObj(
@@ -47,36 +55,47 @@ public class App47 {
         // 7. Fetch and Parse an XML web service
         Document parse = DocumentBuilderFactory.newInstance()
                 .newDocumentBuilder()
-                .parse(new URL("http://www.omdbapi.com/?i=tt0121765&plot=short&r=xml").openStream());
-
+                .parse(URI.create("https://www.omdbapi.com/?i=tt0121765&plot=short&r=xml").toURL().openStream());
+        IO.println(parse.toString());
 
         // 8. Find minimum (or maximum) in a List
-        IntStream.of(14, 35, -7, 46, 98).min();
-        Arrays.asList(14, 35, -7, 46, 98).stream().min(Integer::compare);
-        Arrays.asList(14, 35, -7, 46, 98).stream().reduce(Integer::min);
-        Collections.min(Arrays.asList(14, 35, -7, 46, 98));
+        var min = IntStream.of(14, 35, -7, 46, 98).min();
+        IO.println(min.orElse(Integer.MIN_VALUE));
+        var comp = Stream.of(14, 35, -7, 46, 98).min(Integer::compare);
+        IO.println(comp);
+        var reduce = Stream.of(14, 35, -7, 46, 98).reduce(Integer::min);
+        IO.println(reduce);
+        var min2 = Collections.min(Arrays.asList(14, 35, -7, 46, 98));
+        IO.println(min2);
 
         // 9. Parallel Processing
-        List<String> dataList = null;
-        dataList.parallelStream().map(line -> processItem(line));
+        List<String> dataList = List.of("1", "2", "3");
+        var result = dataList.parallelStream().map(App47::processItem);
+        IO.println(result);
 
         // 10. Sieve of Eratosthenes
-        List<Integer> nums = new LinkedList<>(IntStream.rangeClosed(2, 1000).boxed().collect(Collectors.toList()));
+        List<Integer> nums = IntStream.rangeClosed(2, 1000).boxed().collect(Collectors.toCollection(LinkedList::new));
         IntStream.rangeClosed(2, Double.valueOf(Math.sqrt(nums.getLast())).intValue()).forEach(n -> nums.removeIf(i -> i % n == 0 && n != i));
         nums.forEach(IO::println);
 
         // Bonus: FizzBuzz
         IntStream.rangeClosed(1, 100)
                 .forEach(i -> println(
-                        (i % 3 == 0)
-                            ? ((i % 5 == 0) ? "FizzBuzz" : "Fizz")
-                            : ((i % 5 == 0) ? "Buzz" : i)
-                    )
+                                (i % 3 == 0)
+                                        ? ((i % 5 == 0) ? "FizzBuzz" : "Fizz")
+                                        : ((i % 5 == 0) ? "Buzz" : i)
+                        )
                 );
     }
 
     private static Object processItem(String line) {
         // Some heavy work task
+        try {
+            TimeUnit.MICROSECONDS.sleep(1);
+        } catch (InterruptedException _) {
+            Thread.currentThread().interrupt();
+        }
+        IO.println(line);
         return null;
     }
 }
