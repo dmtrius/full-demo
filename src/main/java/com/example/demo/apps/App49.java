3541f2b0dd69cd080c03b0ea0f2cbdadefc67193
Dmytro Gordiienko <dmtrius@protonmail.com>
2026-05-14 05:47:40 +0200

    update

--- src/main/java/com/example/demo/apps/App49.java
+++ src/main/java/com/example/demo/apps/App49.java
@@ -1,7 +1,20 @@
 package com.example.demo.apps;
 
+import org.springframework.web.reactive.function.client.WebClient;
+
 public class App49 {
     void main() {
         IO.println("App49: Java 26 features demo");
+        m1();
     }
+
+    void m1() {
+        WebClient wc = WebClient.builder().build();
+        var result = wc.get().uri("https://jsonplaceholder.typicode.com/posts/1")
+            .retrieve()
+            .bodyToMono(Message.class);
+        IO.println(result.block());
+    }
+
+    record Message(Integer id, Integer userId, String title, String body){};
 }
