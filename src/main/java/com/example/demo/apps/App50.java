60d7337af393842e33178f45f2813709b3b8576f
Dmytro Gordiienko <dmtrius@protonmail.com>
2026-05-14 15:42:59 +0200

    update

--- src/main/java/com/example/demo/apps/App50.java
+++ src/main/java/com/example/demo/apps/App50.java
@@ -34,9 +34,9 @@ public class App50 {
     private static final int CARDS_PER_RANKS = 4;
     private static final int CARDS_PER_SUIT = 13;
     private static final String SEPARATOR = " | ";
-    public static final String RESET = "\u001B[0m";
-    public static final String RED = "\u001B[31m";
-    public static final String GREEN = "\u001B[32m";
+    private static final String RESET = "\u001B[0m";
+    private static final String RED = "\u001B[31m";
+    private static final String GREEN = "\u001B[32m";
 
     void printDeck(String header, List<Card> list, int lineLength) {
         println(RED + header + RESET + GREEN);
