e36d63f152f24c7834cc472d7fa138fa14e574f2
Dmytro Gordiienko <dmtrius@protonmail.com>
2026-06-02 13:02:55 +0200

    update

--- src/main/java/com/example/demo/apps/App51.java
+++ src/main/java/com/example/demo/apps/App51.java
@@ -3,5 +3,16 @@ package com.example.demo.apps;
 public class App51 {
     void main() {
         IO.println("App51: Java 26 features demo");
+        m1();
+    }
+
+    void m1() {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int maxSum = 0, currSum = 0;
+        for (int num : nums) {
+            currSum = Math.max(num, currSum + num);
+            maxSum = Math.max(maxSum, currSum);
+        }
+        IO.println(maxSum);
     }
 }
