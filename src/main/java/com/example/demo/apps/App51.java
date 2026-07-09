85f70004dbc8addd388443bc22975b5d61e8058e
Dmytro Gordiienko <dmtrius@protonmail.com>
2026-07-09 19:29:41 +0200

    update

--- src/main/java/com/example/demo/apps/App51.java
+++ src/main/java/com/example/demo/apps/App51.java
@@ -1,18 +1,22 @@
 package com.example.demo.apps;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 public class App51 {
     void main() {
-        IO.println("App51: Java 26 features demo");
+        log.info("App51: Java 26 features demo");
         m1();
     }
 
     void m1() {
         int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-        int maxSum = 0, currSum = 0;
+        int maxSum = 0;
+        int currSum = 0;
         for (int num : nums) {
             currSum = Math.max(num, currSum + num);
             maxSum = Math.max(maxSum, currSum);
         }
-        IO.println(maxSum);
+        log.info("Maximum subarray sum: {}", maxSum);
     }
 }
