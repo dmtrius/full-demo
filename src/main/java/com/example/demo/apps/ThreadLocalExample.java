b193791941395e2c09231fb9839c0f61c4d2d36a
Dmytro Gordiienko <dmtrius@protonmail.com>
2026-07-13 20:49:55 +0200

    update

--- src/main/java/com/example/demo/apps/ThreadLocalExample.java
+++ src/main/java/com/example/demo/apps/ThreadLocalExample.java
@@ -3,15 +3,19 @@ package com.example.demo.apps;
 public class ThreadLocalExample {
     private static final ThreadLocal<Integer> threadLocal = ThreadLocal.withInitial(() -> 1);
 
-    public static void main(String[] args) {
-        Runnable task = () -> {
-            System.out.println("Initial Value: " + threadLocal.get());
-            threadLocal.set(threadLocal.get() + 1);
-            System.out.println("Updated Value: " + threadLocal.get());
-        };
-        Thread thread1 = new Thread(task);
-        Thread thread2 = new Thread(task);
-        thread1.start();
-        thread2.start();
+    void main() {
+        try {
+            Runnable task = () -> {
+                IO.println("Initial Value: " + threadLocal.get());
+                threadLocal.set(threadLocal.get() + 1);
+                IO.println("Updated Value: " + threadLocal.get());
+            };
+            Thread thread1 = new Thread(task);
+            Thread thread2 = new Thread(task);
+            thread1.start();
+            thread2.start();
+        } finally {
+            threadLocal.remove();
+        }
     }
 }
