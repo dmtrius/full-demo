0122d72ac0f1362a3f372eb23eca1e078be72568
Dmytro Gordiienko <dmtrius@protonmail.com>
2026-07-13 19:56:26 +0200

    update

--- src/main/java/com/example/demo/apps/TestHttpRequest.java
+++ src/main/java/com/example/demo/apps/TestHttpRequest.java
@@ -1,13 +1,16 @@
 package com.example.demo.apps;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
 
+@Slf4j
 public class TestHttpRequest {
-    public static void main(String[] args) {
+    void main() {
         try {
             // Create a URL object
             URI uri = new URI("https://jsonplaceholder.typicode.com/posts/1");
@@ -21,7 +24,7 @@ public class TestHttpRequest {
 
             // Get the response code
             int responseCode = connection.getResponseCode();
-            System.out.println("Response Code: " + responseCode);
+            IO.println("Response Code: " + responseCode);
 
             // Read the response
             BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
@@ -34,10 +37,10 @@ public class TestHttpRequest {
             in.close();
 
             // Print the response
-            System.out.println("Response: " + response.toString());
+            IO.println("Response: " + response);
 
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("Error occurred while making HTTP request", e);
         }
     }
 }
