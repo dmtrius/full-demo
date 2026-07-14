54f383306aa5080826a3e2fa79b5882d807e1c12
Dmytro Gordiienko <dmtrius@protonmail.com>
2026-07-14 19:48:13 +0200

    update

--- src/main/java/com/example/demo/apps/leetcode/overlapping_intervals/MergeIntervals.java
+++ src/main/java/com/example/demo/apps/leetcode/overlapping_intervals/MergeIntervals.java
@@ -17,8 +17,8 @@ import static java.lang.IO.println;
  * Example 1:
  * <p>
  * Input: intervals = [[1, 3], [2, 6], [8, 10], [15, 18]]
- * Output: [[1,6],[8,10],[15,18]]
- * Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+ * Output: [[1, 6], [8, 10], [15, 18]]
+ * Explanation: Since intervals [1, 3] and [2, 6] overlap, merge them into [1, 6].
  * Example 2:
  * <p>
  * Input: intervals = [[1, 4], [4, 5]]
