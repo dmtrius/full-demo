611e9d601e0f962b75fae40a6dc71e9b0cdaedde
Dmytro Gordiienko <dmitry.gordienko@gmail.com>
2026-07-31 10:18:10 +0200

    update

--- src/main/java/com/example/demo/apps/App53.java
+++ src/main/java/com/example/demo/apps/App53.java
@@ -1,11 +1,7 @@
 package com.example.demo.apps;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 public class App53 {
     void main() {
-        log.info("App53: Java 26 features demo");
-        log.error("App53: ERROR demo");
+
     }
 }
