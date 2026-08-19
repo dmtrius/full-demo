2416a2840d08f3e6716ea1b1444cfa12909244bd
Dmytro Gordiienko <dmtrius@protonmail.com>
2026-08-19 17:13:31 +0200

    update

--- src/main/java/com/example/demo/apps/App53.java
+++ src/main/java/com/example/demo/apps/App53.java
@@ -1,7 +1,19 @@
 package com.example.demo.apps;
 
+import java.math.BigDecimal;
+
 public class App53 {
     void main() {
-        // This method is intentionally left empty
+        BigDecimal n = BigDecimal.TWO.pow(127).subtract(BigDecimal.ONE);
+        IO.println(n);
+        IO.println(isLeapYear(2000));
+        IO.println(isLeapYear(1976));
+        IO.println(isLeapYear(2004));
+        IO.println(isLeapYear(2002));
+    }
+
+    boolean isLeapYear(int year) {
+        IO.println("year: " + year);
+        return ((year * 1073750999L) & 3221352463L) <= 126976;
     }
 }
