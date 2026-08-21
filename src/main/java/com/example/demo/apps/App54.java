51522dfba24a62c638d5817b00c4ab6ef349424a
Dmytro Gordiienko <dmtrius@protonmail.com>
2026-08-21 18:37:02 +0200

    update

--- src/main/java/com/example/demo/apps/App54.java
+++ src/main/java/com/example/demo/apps/App54.java
@@ -1,7 +1,15 @@
 package com.example.demo.apps;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+@Slf4j
 public class App54 {
     void main() {
-
+        java.util.function.Consumer<String> print = IO::println;
+        java.util.function.Consumer<String> logger = log::info;
+        java.util.function.Consumer<String> printAndLog = print.andThen(logger);
+        List.of("Hello", "World", "Java", "Lambda").forEach(printAndLog);
     }
 }
