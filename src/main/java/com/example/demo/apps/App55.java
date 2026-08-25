0d1d63d04e2798429151b3fc8f4aef7d9f49c519
Dmytro Gordiienko <dmtrius@protonmail.com>
2026-08-25 13:55:59 +0200

    update

--- src/main/java/com/example/demo/apps/App55.java
+++ src/main/java/com/example/demo/apps/App55.java
@@ -1,7 +1,22 @@
 package com.example.demo.apps;
 
+import java.io.Console;
+import java.util.Arrays;
+
 public class App55 {
     void main() {
         IO.println("Hello, World!");
+        Console console = System.console();
+        if (null == console) {
+            IO.println("No console available!");
+            return;
+        }
+        String username = console.readLine("Username: ");
+        char[] password = console.readPassword("Password: ");
+
+        IO.println("Username: " + username);
+        IO.println("Password: " + Arrays.toString(password));
+
+        Arrays.fill(password, ' ');
     }
 }
