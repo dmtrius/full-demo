09276db53a482d14db557ba982d307e6a62466d5
Dmytro Gordiienko <dmtrius@protonmail.com>
2026-08-27 13:20:42 +0200

    update

--- src/main/java/com/example/demo/apps/App56.java
+++ src/main/java/com/example/demo/apps/App56.java
@@ -3,5 +3,15 @@ package com.example.demo.apps;
 public class App56 {
     void main() {
         IO.println("Hello!");
+        R1 r1 = new R1(1, "Alice");
+        IO.println(r1);
+        R1 r2 = r1.withName("Bob");
+        IO.println(r2);
+    }
+}
+
+record R1(int id, String name){
+    public R1 withName(String newName) {
+        return new R1(id, newName);
     }
 }
